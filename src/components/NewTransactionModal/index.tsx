a14083de2a6542ed0d6cb9f806167d7641743441
MauricioAires <mauricioaires015@gmail.com>
2023-02-02 16:32:23 -0300

    feat: new transaction modal content

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,37 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+
+import * as S from './styles'
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <S.Overlay />
+      <S.Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <S.CloseButton>
+          <X weight="bold" size={24} />
+        </S.CloseButton>
+
+        <form>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <S.TransactionType>
+            <S.TransactionTypeButton variant="income" type="button">
+              <ArrowCircleUp weight="bold" size={24} />
+              Entrada
+            </S.TransactionTypeButton>
+            <S.TransactionTypeButton variant="outcome" type="button">
+              <ArrowCircleDown weight="bold" size={24} />
+              Saída
+            </S.TransactionTypeButton>
+          </S.TransactionType>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </S.Content>
+    </Dialog.Portal>
+  )
+}
---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,115 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import * as Dialog from '@radix-ui/react-dialog'
+
+export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+
+  background: rgba(0, 0, 0, 0.75);
+`
+
+export const Content = styled(Dialog.Content)`
+  ${({ theme }) => css`
+    min-width: 32rem;
+    border-radius: 6px;
+    padding: 2.5rem 3rem;
+    background: ${theme['gray-800']};
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    form {
+      margin-top: 3rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      input {
+        border-radius: 6px;
+        border: 0;
+        background: ${theme['gray-900']};
+
+        color: ${theme['gray-300']};
+        padding: 1rem;
+
+        &::placeholder {
+          color: ${theme['gray-500']};
+        }
+      }
+
+      button[type='submit'] {
+        height: 58px;
+        background: ${theme['green-500']};
+        color: ${theme['white']};
+        font-weight: bold;
+        padding: 0 1.25rem;
+        border-radius: 6px;
+
+        margin-top: 1.5rem;
+
+        &:hover {
+          background: ${theme['green-700']};
+          transition: background-color 0.2s;
+        }
+      }
+    }
+  `}
+`
+
+export const CloseButton = styled(Dialog.Close)`
+  ${({ theme }) => css`
+    position: absolute;
+    background: transparent;
+    top: 1.5rem;
+    right: 1.5rem;
+
+    line-height: 0;
+
+    color: ${theme['gray-300']};
+  `}
+`
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+const modifiersTransactionTypeButton = {
+  income: (theme: DefaultTheme) => css`
+    svg {
+      color: ${theme['green-300']};
+    }
+  `,
+  outcome: (theme: DefaultTheme) => css`
+    svg {
+      color: ${theme['red-300']};
+    }
+  `
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  ${({ theme, variant }) => css`
+    background: ${theme['gray-700']};
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    border-radius: 6px;
+
+    color: ${theme['gray-100']};
+
+    ${!!variant && modifiersTransactionTypeButton[variant](theme)}
+  `}
+`
