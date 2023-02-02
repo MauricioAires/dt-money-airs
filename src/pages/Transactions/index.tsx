be76f2ef6744a6863c52b402647b8ba491f30562
MauricioAires <mauricioaires015@gmail.com>
2023-02-02 11:55:35 -0300

    feat: transaction page base

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+
+import * as S from './styles'
+
+export function Transactions() {
+  return (
+    <S.TransactionsWrapper>
+      <Header />
+    </S.TransactionsWrapper>
+  )
+}
